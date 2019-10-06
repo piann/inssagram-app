06a52231e3280837143608407321ede3a37eb75c
root <root@localhost.localdomain>
2019-10-06 03:55:55 -0400

    use apollo cache (elementary)

--- App.js
+++ App.js
@@ -3,17 +3,33 @@ import { Ionicons } from "@expo/vector-icons";
 import { AppLoading } from "expo";
 import * as Font from 'expo-font';
 import {Asset} from 'expo-asset';
-import { Text, View } from "react-native";
+import { Text, View, AsyncStorage } from "react-native";
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {persistCache} from 'apollo-cache-persist';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo-hooks';
+import apolloClientOpts from './apollo';
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
   const preLoad = async () => {
     try {
       await Font.loadAsync({
         ...Ionicons.font
       });
       await Asset.loadAsync([require("./assets/logo.png")]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOpts
+      });
       setLoaded(true);
+      setClient(client);
     } catch (e) {
       console.log(e);
     }
@@ -22,10 +38,13 @@ export default function App() {
     preLoad();
 
   }, []);
-  return loaded ? (
+  return loaded && client ? (
+    <ApolloProvider client={client}>
+
     <View>
       <Text>App.js is working !</Text>
     </View>
+  </ApolloProvider>
   ) : (
     <AppLoading />
   );
---
+++ apollo.js
@@ -0,0 +1,5 @@
+const apolloClientOpts = {
+    uri:"http://localhost:4000/graphql"
+}
+
+export default apolloClientOpts;
