45fce534bbac74d51714b9eee15926851fba7cfd
root <root@localhost.localdomain>
2019-10-07 07:24:47 -0400

    use navigation for screen related auth

---
+++ navigation/AuthNavigation.js
@@ -0,0 +1,20 @@
+import { createStackNavigator} from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import Signup from "../screens/Auth/Signup"
+import Login from "../screens/Auth/Login"
+import Confirm from "../screens/Auth/Confirm"
+import AuthHome from "../screens/Auth/AuthHome"
+
+
+const AuthNavigation = createStackNavigator(
+    {
+        AuthHome,
+        Signup, 
+        Confirm,
+        Login
+    }, 
+    {
+        initialRouteName:"AuthHome"
+    });
+
+export default createAppContainer(AuthNavigation)
