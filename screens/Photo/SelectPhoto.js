1204682add29a5ae73bc81ca444030ef1462a526
DongSu <piann@DongSus-iMac.local>
2019-11-03 17:44:44 +0900

    ask permission and get photo from mediaLibrary

--- screens/Photo/SelectPhoto.js
+++ screens/Photo/SelectPhoto.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import * as Permissions from "expo-permissions";
+import { Image, ScrollView, TouchableOpacity } from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import Loader from "../../components/Loader";
+import constants from "../../constants";
+import styles from "../../styles";
+
 
  const View = styled.View`
   justify-content: center;
@@ -9,8 +16,49 @@ import styled from "styled-components";
 
  const Text = styled.Text``;
 
- export default () => (
+ export default () => {
+  const [loading, setLoading] = useState(true);
+  const [hasPermission, setHasPermission] = useState(false);
+  const [selected, setSelected] = useState();
+  const [allPhotos, setAllPhotos] = useState();
+  const getPhotos = async() => {
+    try{
+      const { assets } = await MediaLibrary.getAssetsAsync({
+        sortBy: [[MediaLibrary.SortBy.creationTime, false]]
+      });
+      const [firstPhoto] = assets;
+      console.log(firstPhoto);
+      setSelected(firstPhoto);
+      setAllPhotos(assets);
+
+    }catch(e){
+      console.log(e);
+    }
+  }
+  const askPermission = async()=>{
+    try{
+      const permission = await Permissions.getAsync(Permissions.CAMERA_ROLL);
+      console.log(permission);
+      const {status} = permission;
+      if (status === "granted"){
+        await getPhotos();
+        console.log(selected);
+        setHasPermission(true);
+      }
+    } catch(e){
+      console.log(e)
+      setHasPermission(false);
+    }
+  };
+  useEffect(()=>{
+    askPermission();
+  }, []);
+
+  return(
+
   <View>
-    <Text>Select Photo</Text>
+    {hasPermission? <Image style={{width:150, height:150}} source={{uri:selected.uri}}/>
+    : <Text>"U have to grant permssion"</Text>}
   </View>
-);
+  )
+ };
