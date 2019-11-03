d13d984cdfcb7e0b70aaabdd47dcdb22879bb334
DongSu <piann@DongSus-iMac.local>
2019-11-03 16:59:40 +0900

    add primitive feature of PhotoNavigation

--- navigation/PhotoNavigation.js
+++ navigation/PhotoNavigation.js
@@ -4,24 +4,58 @@ import TakePhoto from "../screens/Photo/TakePhoto"
 import UploadPhoto from "../screens/Photo/UploadPhoto"
 import { createStackNavigator } from "react-navigation-stack";
 import { stackStyles } from "./config";
+import styles from "../styles";
 
 const PhotoTabs = createMaterialTopTabNavigator({
-    SelectPhoto,
-    TakePhoto
+    Select:{
+        screen:SelectPhoto,
+        navigationOptions:{
+            tabBarLabel: "Select"
+        }
+    },
+
+    Take:{
+        screen:TakePhoto,
+        navigationOptions:{
+            tabBarLabel: "Take"
+        }
+    },
 },
 {
     tabBarPosition:"bottom",
-    headerMode:"none",
-    defaultNavigationOptions: {
-    header:null,
-    headerStyle:{
-        ...stackStyles
-    }
-    }
+    tabBarOptions:{
+        indicatorStyle:{
+            backgroundColor:styles.blackColor,
+            marginBottom:5
+        },
+        labelStyle:{
+            color:styles.blackColor,
+            fontWeight:"600"
+        },
+        style:{
+            paddingBottom:15,
+            ...stackStyles
+        }
+    },
+
 }
 )
 
 export default createStackNavigator({
-    PhotoTabs,
+    Tabs:{
+        screen: PhotoTabs,
+        navigationOptions:{
+            header: null
+        }
+    },
     UploadPhoto
-});
+},
+{
+    defaultNavigationOptions:{
+        headerStyle:{
+            ...stackStyles
+        }
+    }
+}
+
+);
